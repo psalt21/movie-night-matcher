72987f78c4b186cca34891fccb1eb8dffef51a58
Phil Salter <phil@goreact.com>
2020-08-27 16:58:56 -0600

    improve some styling and add review modal to movie info modal

--- src/app/modals/movie-info-modal/movie-info-modal.component.ts
+++ src/app/modals/movie-info-modal/movie-info-modal.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { MovieReviewModalComponent } from '../movie-review-modal/movie-review-modal.component';
+import { singleMovieData } from 'src/app/test-data-individual-movie';
+// import { MovieCardComponent } from '../../movie-card/movie-card.component';
 
 @Component({
   selector: 'app-movie-info-modal',
@@ -7,15 +10,30 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./movie-info-modal.component.scss'],
 })
 export class MovieInfoModalComponent implements OnInit {
-  
-  constructor(public modalCtrl: ModalController ) { }
+
+  constructor(public modalController: ModalController ) { }
 
   ngOnInit() {}
+
+  openReviewModal() {
+    this.presentReviewModal();
+  }
+
+  async presentReviewModal() {
+    const modal = await this.modalController.create({
+      component: MovieReviewModalComponent,
+      cssClass: 'my-custom-class',
+      componentProps: {
+        movieData: singleMovieData
+      }
+    });
+    return await modal.present();
+  }
 
   dismiss() {
     // using the injected ModalController this page
     // can "dismiss" itself and optionally pass back data
-    this.modalCtrl.dismiss({
+    this.modalController.dismiss({
       dismissed: true
     });
   }
